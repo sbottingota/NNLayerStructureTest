66c2d1660c9ee62eccd1e9e85eb3516d70aad451
sbottingota <saku.bottingota@gmail.com>
2023-01-12 12:29:15 +0000

    Made the program train, validate and print useful data about each of the layer sizes.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from sklearn.neural_network import MLPClassifier
+from keras.datasets import mnist
+import numpy as np
+
 import pickle
+import itertools
 
-def generatorToList(generator):
-    output = []
-    for item in generator:
-        output.append(item)
-
-    return output
+a = np.array((1, 2))
 
 layerSizes = []
 
@@ -16,20 +15,49 @@ for i in range(1, 5):
     layerSizes.append([1024 for j in range(i)])
 
     if i > 1:
-        increasingLayer = [1024]
+        increasingLayer = [512]
         for j in range(i - 1):
-            increasingLayer.append(increasingLayer[j] / 2)
+            increasingLayer.append(increasingLayer[j] * 2)
 
-        decreasingLayer = [512]
+        decreasingLayer = [1024]
         for j in range(i - 1):
-            decreasingLayer.append(decreasingLayer[j - 1] * 2)
+            decreasingLayer.append(decreasingLayer[j] // 2)
 
-        layerSizes.append(generatorToList(increasingLayer))
-        layerSizes.append(generatorToList(decreasingLayer))
+        layerSizes.append(increasingLayer)
+        layerSizes.append(decreasingLayer)
 
 layerSizes.append([4096])
 
 layerSizes.append([1 for i in range(10)])
 layerSizes.append([128 for i in range(10)])
 
-print(layerSizes)
+(xTrain, yTrain), (xTest, yTest) = mnist.load_data()
+
+xTrain = np.reshape(xTrain, (len(xTrain), 28 ** 2))
+xTest = np.reshape(xTest, (len(xTest), 28 ** 2))
+
+for layerSize in layerSizes:
+    clf = MLPClassifier(hidden_layer_sizes=layerSize, learning_rate="invscaling", verbose=True, max_iter=200)
+    clf.fit(xTrain, yTrain)
+
+    predictions = clf.predict(xTest)
+
+    nRight = nWrong = 0
+
+    for i in range(len(predictions)):
+        if predictions[i] == yTest[i]:
+            nRight += 1
+
+        else:
+            nWrong += 1
+
+    print("Architecture:", layerSize)
+    print("Accuracy:", nRight / (nRight + nWrong))
+
+    weights = len(list(itertools.chain(*list(itertools.chain(*clf.coefs_)))))
+    biases = len(list(itertools.chain(*clf.coefs_)))
+    params = weights + biases
+
+    print("Parameters:", params)
+
+    pickle.dump(clf, open("networks/" + str(layerSize) + ".pkl", "wb"))
