b8239c3e1a017db70cdedc4d6bf9e3a1fdcf9399
sbottingota <saku.bottingota@gmail.com>
2023-01-13 08:15:03 +0000

    Created a shell file to install pip and modules that the program needs.

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#this assumes that you already have python 3 installed
+python -m ensurepip --upgrade #install pip
+python -m pip install --upgrade pip #upgrade pip if it is an old version
+
+#install modules
+python -m pip install --upgrade keras
+python -m pip install --upgrade numpy
+python -m pip install --upgrade skit-learn
